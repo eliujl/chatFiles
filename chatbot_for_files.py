4e7c713d8a2f457fa410c383e1c43b0e3933b826
ZenNE <eliujl@gmail.com>
2023-04-30 22:55:09 -0500

    Create chatbot_for_files.py
    chatbot with multiple doc/pdf/txt files, with or without pinecone, ingesting new files or using existing vectorstores

---
+++ chatbot_for_files.py
@@ -0,0 +1,154 @@
+# chatbot using Langchain, OpenAI (API key required),
+# and existing Pinecone index (API key required).
+
+# Import required libraries
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import UnstructuredWordDocumentLoader, UnstructuredPDFLoader, UnstructuredFileLoader
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.chat_models import ChatOpenAI
+from langchain.vectorstores import Pinecone, Chroma
+from langchain.chains import ConversationalRetrievalChain
+from colorama import init, Fore, Style
+import os
+import pinecone
+
+# Set up OpenAI API key (from .bashrc, Windows environment variables, .env)
+OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
+
+# Set up OpenAI embeddings
+embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+
+# Use Open AI LLM with gpt-3.5-turbo.
+# Set the temperature to be 0 if you do not want it to make up things
+llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo",
+                 openai_api_key=OPENAI_API_KEY)
+
+
+# Set up Pinecone env
+PINECONE_API_KEY = os.environ['PINECONE_API_KEY']
+PINECONE_API_ENV = os.environ['PINECONE_API_ENV']
+pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_API_ENV)
+
+# Get user input of whether to use Pinecone or not
+r = input('Do you want to use Pinecone index? (y/n): ')
+if r.lower() == 'y' and PINECONE_API_KEY != '':
+    pinecone_index_name = input(
+        Fore.GREEN + "Enter your Pinecone index: " + Style.RESET_ALL)
+    use_pinecone = True
+else:
+    print('Not using Pinecone or empty Pinecone API key provided. Using Chroma instead')
+    use_pinecone = False
+    persist_directory = "./vectorstore"
+
+# Get user input of whether to ingest files or using existing vector store
+r = input('Do you want to ingest the file(s) in ./docs/? (y/n): ')
+if r.lower() == 'y':
+
+    file_path = './docs/'
+    all_texts = []
+    n_files = 0
+    n_char = 0
+    n_texts = 0
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=400, chunk_overlap=50)
+    for filename in os.listdir(file_path):
+        file = os.path.join(file_path, filename)
+        if os.path.isfile(file):
+            if file.endswith('.docx'):
+                loader = UnstructuredWordDocumentLoader(file)
+            elif file.endswith('.txt'):
+                loader = UnstructuredFileLoader(file)
+            elif file.endswith('.pdf'):
+                loader = UnstructuredPDFLoader(file)
+            else:
+                continue
+            data = loader.load()
+            texts = text_splitter.split_documents(data)
+            n_files += 1
+            n_char += len(data[0].page_content)
+            n_texts += len(texts)
+            all_texts.extend(texts)
+    print(
+        f'Loaded {n_files} file(s) with {n_char} characters, and split into {n_texts} split-documents.')
+
+    if use_pinecone:
+        docsearch = Pinecone.from_texts(
+            [t.page_content for t in all_texts], embeddings, index_name=pinecone_index_name)  # add namespace=pinecone_namespace if provided
+    else:
+        docsearch = Chroma.from_documents(
+            all_texts, embeddings, collection_name="my_collection", persist_directory="./vectorstore")
+else:
+    if use_pinecone:
+        # Load the pre-created Pinecone index.
+        # The index which has already be stored in pinecone.io as long-term memory
+
+        if pinecone_index_name in pinecone.list_indexes():
+            docsearch = Pinecone.from_existing_index(
+                pinecone_index_name, embeddings)  # add namespace=pinecone_namespace if provided
+            index_client = pinecone.Index(pinecone_index_name)
+            # Get the index information
+            index_info = index_client.describe_index_stats()
+            namespace_name = ''
+            n_texts = index_info['namespaces'][namespace_name]['vector_count']
+        else:
+            raise ValueError('''Cannot find the specified Pinecone index.
+							Create one in pinecone.io or using 
+							pinecone.create_index(
+								name=index_name, dimension=1536, metric="cosine", shards=1)''')
+    else:
+        docsearch = Chroma(persist_directory=persist_directory, embedding_function=embeddings,
+                           collection_name="my_collection")
+        n_texts = docsearch._client._count(collection_name="my_collection")
+
+# print(len(docsearch))
+# Ask if the source document should be printed out.
+# If yes, the ouptput could be very long
+print_source = input(Fore.GREEN +
+                     "You want to also print texts from one most relevant source document? y/n: "
+                     + Style.RESET_ALL)
+
+# number of sources (split-documents when ingesting files); default is 4
+k = min([20, n_texts])
+
+# Set up retriever
+retriever = docsearch.as_retriever(
+    search_type="similarity", search_kwargs={"k": k})
+
+# Set up ConversationalRetrievalChain
+CRqa = ConversationalRetrievalChain.from_llm(
+    llm, retriever=retriever, return_source_documents=True)
+
+
+def chatbot_loop():
+    # Get a response from the retriever based on the user input and chat history
+    def get_response(query, chat_history):
+        result = CRqa({"question": query, "chat_history": chat_history})
+        return result['answer'], result['source_documents']
+
+    # Initialize chat history
+    chat_history = []
+    # Initialize color
+    init()
+    # Start the chat loop
+    while True:
+        # Get user input
+        query = input(
+            Fore.GREEN + "Enter your question; enter 'exit' to exit: " + Style.RESET_ALL)
+        if query.lower() == 'exit':
+            break
+        # Generate a reply based on the user input and chat history
+        reply, source = get_response(query, chat_history)
+        print(reply)
+        # Print the first (most relevant) source
+        # (only the first 400 characters for brevity here) if needed
+        if print_source.lower() == 'y':
+            print(Fore.GREEN + "Source document content:\n" +
+                  Style.RESET_ALL,
+                  source[0].page_content[:400])
+
+        # Update the chat history with the user input and system response
+        chat_history.append((query, reply))
+
+
+chatbot_loop()
